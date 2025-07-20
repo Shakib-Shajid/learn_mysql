9808341d9aee15694b6d56a56e7e3e00a8e5d9ee
shakib-shajid <shakibalshajid1682@gmail.com>
2025-07-20 17:48:33 +0600

    read added

--- app.js
+++ app.js
@@ -5,22 +5,43 @@ const db = await mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
-    // database: "mysql_db",
+    database: "mysql_db",
 })
 
+console.log("database connected");
 
 // 2. create DB
 
-await db.execute (`create database mysql_db`);
+// await db.execute (`create database mysql_db`);
 
-console.log(await db.execute('show databases'));
+// console.log(await db.execute('show databases'));
 
 // ........................................................
 
 //  3. create a table
 
+// await db.execute(`
+//     CREATE TABLE users (
+//     id INT 
+//     AUTO_INCREMENT PRIMARY KEY, 
+//     username VARCHAR(100) NOT NULL, 
+//     email VARCHAR(100) NOT NULL UNIQUE);
+//     `)
 
 
 
- 
 //  4. perform CRUD 
+
+// insert
+
+// await db.execute(`
+//     insert into users(username, email) values('abcd', "abcd@gmail.com")
+//     `);
+
+
+
+// read
+const [rows] = await db.execute(` select * from users `)
+console.log(rows);
+// [rows field]
+
