16773c15f023cdaa3f81a9d917541eca3077afc1
shakib-shajid <shakibalshajid1682@gmail.com>
2025-07-20 17:24:47 +0600

    connected

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
+import mysql from "mysql2/promise";
+
+//   1. connect o mysql server
+const db = await mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "",
+})
+
+
+
 console.log("hello");
 
+
+
+
+/**
+ * 2. create DB
+ * 3. create a table
+ * 4. perform CRUD 
+ * */ 
